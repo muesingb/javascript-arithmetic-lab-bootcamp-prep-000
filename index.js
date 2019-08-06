0b2c5b8123d5130d61e06478eab4a57cc0f9feb7
Barbara Muesing <muesingb@gmail.com>
2019-08-06 19:15:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,19 +10,23 @@ function subtract(a, b) {
 function multiply(a, b) {
   return a*b
 }
-//
+//divide(a, b) divides a by b and returns the result
 function divide(a, b) {
   return a/b
 }
+//inc(n) increments n and returns the result
 function inc(n) {
   return n+= 1
 }
+//dec(n) decrements n and returns the result
 function dec(n) {
   return n-= 1
 }
+//parses n as an integer and returns the parsed integer, assumes base 10, returns NaN as appropriate
 function makeInt(n) {
   return parseInt(n, 10)
 }
+//preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number, returns NaN as appropriate
 function preserveDecimal(n) {
   return parseFloat(n)
 }
